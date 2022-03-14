69a8046ad60703eab31f073be73bd5b23f24f242
Joao Oliveira <jmlcoliveira@gmail.com>
2022-03-14 12:49:27 +0000

    new stuff

--- Práticas/aula2.ml
+++ Práticas/aula2.ml
@@ -2,6 +2,7 @@ let rec succAll l =
 	match l with 
 	| [] -> []
 	| x::xs -> x+1 :: succAll xs
+;;
 
 succAll [1; 2; 4; 8; 16; 32; 64; 128; 256; 512; 1024]
 
@@ -9,34 +10,55 @@ let rec belongs v l =
 	match l with
 	| [] -> false
 	| x::xs -> if v=x then true else belongs v xs
+;;
 
 belongs 4 [1;2;3;4;5;6]
 belongs 7 [1;2;3;4;5;6]
 
-let rec contains v l =
-	match l with 
-	| [] -> false
-	| x::xs -> if v = x then true else contains v xs
-
 let rec union l1 l2 = 
 	match l1 with
-	| [] -> []
-	| x::xs -> x:: union l2 xs
+	| [] -> l2
+	| x::xs -> if belongs x l2 then union xs l2 else x:: union xs l2 
+;;
 
-union [7;3;9] [2;1;9]
+union [7;3;9] [2;1;9];;
 
 let rec inter l1 l2 =
 	match l1 with
 	| [] -> []
-	| x::xs -> if contains x l2 then x:: inter xs l2 else inter xs l2
+	| x::xs -> if belongs x l2 then x :: inter xs l2 else inter xs l2
+;;
 
 inter [7;3;9; 0] [2;1;9; 0]
 
-let rec diff l1 l2 =
-	match l1 with
-	| [] -> []
-	| x::xs -> if contains x l2 then inter xs l2 else x:: inter xs l2
+	let rec diff l1 l2 =
+		match l1 with
+		| [] -> []
+		| x::xs -> if belongs x l2 then diff xs l2 else x:: diff xs l2
+	;;
 
  diff [7;3;9] [2;1;9]
 
+let rec add x l =
+	match l with
+		| [] -> []
+		| s::l -> (x::s)::add x l
+;;
 
+let rec power l1 =
+	match l1 with
+		| [] -> [[]]
+		| x::xs -> add x (power xs) @ power xs
+;;
+
+let rec nat x =
+	if x = 0 then []
+	else x-1 :: nat (x-1)
+;;
+
+
+
+let rec pack l =
+	match l with
+	 | [] -> []
+	 | x::xs -> 
